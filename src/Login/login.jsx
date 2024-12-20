dc9e682c4d69020f1c62da37ea47b73fe31782b5
mayatarek <Mayatarek@iCloud.com>
2024-12-20 20:57:51 +0200

    Added login and signup

---
+++ src/Login/login.jsx
@@ -0,0 +1,38 @@
+import {useForm} from 'react-hook-form'
+import { useNavigate } from 'react-router-dom';
+
+function Login(){
+    //Some user account API thing idk
+
+    const {register, handleSubmit} = useForm();
+    const Navigate = useNavigate();
+    // const onSubmit = (data) => {
+        
+    // }
+
+    return(
+        <div class="card">
+            <form>
+                <lable htmlFor="email">Email</lable><br/>
+                <input type="email" placeholder="johndoe@gmail.com" id="email" {...register("email",{
+                pattern:{
+                    value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: 'Invalid email'
+                }})}/>
+                <br/>
+                <lable htmlFor="password">Password</lable><br/>
+                <input type="password" placeholder="" id="password"/>
+                <br/>
+                <button>submit</button>
+                
+            </form>
+            
+            <button onClick={() =>{
+                Navigate('/signup')
+            }}>New user? Sign up!</button>
+            
+        </div>
+    )
+}
+
+export default Login
