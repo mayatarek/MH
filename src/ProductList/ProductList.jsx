5181c4d99bd37ce8828357f73ff253969eeccb1c
mayatarek <Mayatarek@iCloud.com>
2024-12-20 21:08:55 +0200

    fixed file names and added some pictures

---
+++ src/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+function ProductList(){
+    var products
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductList
